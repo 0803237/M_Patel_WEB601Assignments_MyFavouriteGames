84ec0b0e62a1a1e364d9fd3e83a5bf941e29fcee
Moksha Patel <w0803237@myscc.ca>
2023-04-21 19:04:38 -0500

    updated

--- M_Patel_MyFavouriteGames/src/app/content-card/content-card.component.ts
+++ M_Patel_MyFavouriteGames/src/app/content-card/content-card.component.ts
@@ -1,31 +1,3 @@
-// import { Component, Input } from '@angular/core';
-// import { Content} from '../helper-files/content-interface';
-
-// @Component({
-//   selector: 'app-content-card',
-//   templateUrl: './content-card.component.html',
-//   styleUrls: ['./content-card.component.scss']
-// })
-// export class ContentCardComponent {
-//   title = 'Moksha Patel';
-//   @Input() game?: Content;
-
-//   printDataOnConsole()
-//   {
-//     console.log(`Content's id is "${this.game?.id}" and Title of the game is "${this.game?.title}"`);
-//   }
-
-  
-//   ngOnInit(): void {
-//   }
-
-//   constructor() {
-  
-    
-//   }
-// }
-
-
 import { Component } from '@angular/core';
 
 @Component({
